90c97721f946f2b5899a11d166fb388d2c95fce7
JohannesGrosseBoymann <dohecaf1219@gmail.com>
2024-11-07 12:26:55 +0100

    do challenges

--- js-basics-and-variables/web-exercises-sessions-js-basics-and-variables-social-media-post/index.js
+++ js-basics-and-variables/web-exercises-sessions-js-basics-and-variables-social-media-post/index.js
@@ -10,7 +10,11 @@ console.clear();
 */
 
 // --v-- write your code here --v--
-
+const title = "Message from Josh";
+const text = "Today I did a run in the morning";
+let numberOfLikes = 23;
+const authorName = "Johannes Große Boymann";
+let isReported = false;
 // --^-- write your code here --^--
 
 /*
@@ -18,5 +22,11 @@ console.clear();
 */
 
 // --v-- write your code here --v--
-
+console.log(title);
+console.log(text);
+console.log("Number of likes: ", numberOfLikes);
+console.log(authorName);
+console.log(isReported);
+numberOfLikes++;
+console.log("Number of likes increased: ", numberOfLikes);
 // --^-- write your code here --^--

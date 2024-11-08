a5595fe687f129c33c6e0f75ef023b415949cb1b
JohannesGrosseBoymann <dohecaf1219@gmail.com>
2024-11-08 16:40:01 +0100

    add weather condition in if switch statement

--- js-conditions-and-booleans/js-conditions-and-booleans_weather-check/js/index.js
+++ js-conditions-and-booleans/js-conditions-and-booleans_weather-check/js/index.js
@@ -1,5 +1,35 @@
 // Change this value to test different "weather" conditions.
-const weather = "rainy"
+const weather = "sunny"
 
-// Adjust the "temperature" value to trigger different messages (for Part 2 only).
-const temperature = 22; 
+switch (weather) {
+    case "sunny":
+        console.log("It`s sunny");
+        break;
+    case "rainy":
+        console.log("It`s raining");
+        break;
+    case "snowy":
+        console.log("It`s snowing, stay warm");
+        break;
+    default:
+        console.log("I'm not sure what the weather is like.");
+}
+
+// Adjust thconsole.log("It`s sunny")e "temperature" value to trigger different messages (for Part 2 only).
+const temperature = 15; 
+
+switch (weather) {
+    case "sunny":
+        (temperature >20) ? console.log("It`s sunny and warm!") :
+        console.log("It`s sunny, but a bit chilly!");
+        break;
+    case "rainy":
+        console.log("It`s raining, don`t forget your umbrella!");
+        break;
+    case "snowy":
+        (temperature < 0) ? console.log("Freezing snow!")
+        : console.log("It`s snowing, stay warm");
+        break;
+    default:
+        console.log("I'm not sure what the weather is like.");
+}

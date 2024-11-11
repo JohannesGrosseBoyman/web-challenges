7d387b79d5f79f135ef03769411dad48e08213af
JohannesGrosseBoymann <dohecaf1219@gmail.com>
2024-11-11 12:20:12 +0100

    add assignment in objects

--- js-objects-and-arrays/js-objects-and-arrays_objects/js/index.js
+++ js-objects-and-arrays/js-objects-and-arrays_objects/js/index.js
@@ -4,7 +4,9 @@ console.clear();
 // Create an object `personWithNameAgeEmail` with the properties "name", "age" and "email".
 
 const personWithNameAgeEmail = {
-  example: "example",
+  name: "Johannes",
+  age: 60,
+  email: "example@test.com",
 };
 
 // EXERCISE 2
@@ -15,9 +17,9 @@ const personAlex = {
   age: 24,
 };
 
-const nameOfAlex = "Change me";
-const ageOfAlex = "Change me";
-
+const nameOfAlex = personAlex.name;
+const ageOfAlex = personAlex.age;
+console.log(nameOfAlex, ageOfAlex);
 // EXERCISE 3
 // Change the `name` to "Alex" and `age` to "35" by updating the `personToChange`object: `person.existingProperty = newValue`.
 
@@ -26,6 +28,10 @@ const personToChange = {
   age: 24,
 };
 
+personToChange.name = "Alex";
+personToChange.age = 35;
+
+console.log(personToChange);
 // EXERCISE 4
 // Add the properties `age` with value "5" and `breed` with value "husky" to the `petPluto` object: `pet.newProperty = newValue`.
 
@@ -33,6 +39,10 @@ const petPluto = {
   name: "Pluto",
   species: "dog",
 };
+
+petPluto.age = 5;
+petPluto.breed = "husky";
+console.log(petPluto);
 
 export {
   personWithNameAgeEmail,

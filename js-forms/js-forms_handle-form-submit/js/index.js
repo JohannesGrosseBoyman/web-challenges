d8d71c4693c5414427cdcac70f0141ca7670b61a
JohannesGrosseBoymann <dohecaf1219@gmail.com>
2024-11-13 11:34:52 +0100

    create Data-Object from Input

--- js-forms/js-forms_handle-form-submit/js/index.js
+++ js-forms/js-forms_handle-form-submit/js/index.js
@@ -1,3 +1,17 @@
 console.clear();
 
 const form = document.querySelector('[data-js="form"]');
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData);
+  console.log("data", data);
+  console.log(
+    `The age-badness-sum of ${data.firstName} is ${
+      parseInt(data.age) + parseInt(data.badness)
+    }`
+  );
+  event.target.elements.firstName.focus();
+  event.target.reset();
+});

177b28eeb5399bbca1ac029a00e6cc0b79566de8
JohannesGrosseBoymann <dohecaf1219@gmail.com>
2024-11-13 15:41:17 +0100

    add functinality to add and clear button

--- js-createElement/js-createelement_toast-messages/js/index.js
+++ js-createElement/js-createelement_toast-messages/js/index.js
@@ -4,10 +4,18 @@ const toastContainer = document.querySelector('[data-js="toast-container"]');
 const addButton = document.querySelector('[data-js="add-button"]');
 const clearButton = document.querySelector('[data-js="clear-button"]');
 
-addButton.addEventListener("click", () => {
+addButton.addEventListener("click", (event) => {
   // Exercise: Append a new entry to the toast messages container
+  event.preventDefault();
+  const newEntry = document.createElement("li"); //1
+  toastContainer.appendChild(newEntry); //2
+  newEntry.classList.add("toast-container__message");
+  newEntry.textContent = "I am another toast message";
 });
 
-clearButton.addEventListener("click", () => {
+clearButton.addEventListener("click", (event) => {
   // Exercise: Clear the stack of toast messages
+  event.preventDefault();
+  while (toastContainer.hasChildNodes)
+    toastContainer.removeChild(toastContainer.children[0]);
 });

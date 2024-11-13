46a62a6223e7015d1021d438f303e2dc05860687
JohannesGrosseBoymann <dohecaf1219@gmail.com>
2024-11-13 16:37:54 +0100

    replicate social medis post

--- js-createElement/js-createelement_social-media-post/js/index.js
+++ js-createElement/js-createelement_social-media-post/js/index.js
@@ -10,3 +10,28 @@ likeButton.addEventListener("click", handleLikeButtonClick);
 
 // Exercise:
 // Use document.createElement() and append another social media post to the body.
+const newPost = document.createElement("article");
+newPost.classList.add("post");
+const newParagraph = document.createElement("p");
+newParagraph.classList.add("post__content");
+newParagraph.textContent =
+  "Lorem ipsum dolor, sit amet consectetur adipisicing elit.";
+const newFooter = document.createElement("footer");
+newFooter.classList.add("post__footer");
+const newSpan = document.createElement("span");
+newSpan.classList.add("post__username");
+newSpan.textContent = "@username";
+const newButton = document.createElement("button");
+newButton.classList.add("post__button");
+newButton.textContent = " ♥ Like";
+
+newFooter.appendChild(newSpan);
+newFooter.appendChild(newButton);
+
+newPost.appendChild(newParagraph);
+newPost.appendChild(newFooter);
+
+const body = document.querySelector("body");
+body.appendChild(newPost);
+
+newButton.addEventListener("click", handleLikeButtonClick);

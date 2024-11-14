85f11674f49d05ade107ca3ceccb639ed22b836a
JohannesGrosseBoymann <dohecaf1219@gmail.com>
2024-11-14 15:06:29 +0100

    add user input filled stars

--- js-loops/js-loops_stars/js/index.js
+++ js-loops/js-loops_stars/js/index.js
@@ -2,13 +2,28 @@ console.clear();
 
 const starContainer = document.querySelector('[data-js="star-container"]');
 
-function renderStars() {
+function renderStars(filledStars) {
   // Reset the star container before re-rendering stars
   starContainer.innerHTML = "";
 
   // --v-- write or modify code below this line --v--
 
-  // --^-- write or modify code above this line --^--
+  for (let i = 1; i < 6; i++) {
+    const img = document.createElement("img");
+    const type = i <= filledStars ? "filled" : "empty";
+    img.src = `assets/star-${type}.svg`;
+    img.addEventListener("click", () => {
+      renderStars(i);
+    });
+    /*  if (i <= filledStars) {
+        img.src =  `assets/star-filled.svg`
+        starContainer.appendChild(img);
+      } else {
+        img.src = `assets/star-empty.svg`; */
+    starContainer.appendChild(img);
+  }
 }
+
+// --^-- write or modify code above this line --^--
 
 renderStars();

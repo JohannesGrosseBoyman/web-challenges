e85a2a8cc125412381c0a5ddd24f6c956f2598c1
JohannesGrosseBoymann <dohecaf1219@gmail.com>
2024-11-25 15:51:52 +0100

    create Greeting function

--- react-props/react-props_greeting/src/App.jsx
+++ react-props/react-props_greeting/src/App.jsx
@@ -1,5 +1,18 @@
 import "./styles.css";
 
+// Props
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return (
+    <div>
+      <Greeting name="Klaus" />
+      <Greeting name="Johannes" />
+    </div>
+  );
+}
+
+// component
+function Greeting({ name }) {
+  const coaches = ["Andrea", "Esraa", "Gemima", "Klaus", "Peter"];
+  const isCoach = coaches.includes(name);
+  return <h1>Hello {isCoach ? "Coach" : name}!</h1>;
 }

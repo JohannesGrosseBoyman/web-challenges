22abcdc41bf6290b06f8bb8135d52f354649c48b
JohannesGrosseBoymann <dohecaf1219@gmail.com>
2024-11-26 10:26:17 +0100

    use the children prop in Button

--- react-nesting/react-nesting_buttons-and-children/src/App.jsx
+++ react-nesting/react-nesting_buttons-and-children/src/App.jsx
@@ -4,18 +4,18 @@ import "./styles.css";
 export default function App() {
   return (
     <main>
-      <Button />
-      <Button />
-      <Button />
-      <Button />
+      <Button>A way to go</Button>
+      <Button>Where are you heading for?</Button>
+      <Button>Do you like to take this way?</Button>
+      <Button>Or do you want to pause?</Button>
     </main>
   );
 }
 
-function Button() {
+function Button({children}) {
   return (
     <button className="button" type="button">
-      Click me!
+      {children}
     </button>
   );
 }

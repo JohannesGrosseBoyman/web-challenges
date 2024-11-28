29dc8651a0cfc929d8e1feb266ea4e5409ea24c1
JohannesGrosseBoymann <dohecaf1219@gmail.com>
2024-11-28 19:28:16 +0100

    add functionality to Favourite Button

--- react-state-3/react-state-3_journal-app-favorite-button-shared-state/src/components/EntriesSection/index.jsx
+++ react-state-3/react-state-3_journal-app-favorite-button-shared-state/src/components/EntriesSection/index.jsx
@@ -6,12 +6,12 @@ import Tab from "../Tab";
 import Badge from "../Badge";
 import { Fragment } from "react";
 
-export default function EntriesSection({ entries }) {
+export default function EntriesSection({ entries, onToggleFavorite }) {
   return (
     <section className="entries-section">
       <Tabs>
         <Tab active>
-          All Entries <Badge isActive>3</Badge>
+          All Entries <Badge isActive>{entries.length}</Badge>
         </Tab>
         <Tab>
           Favorites <Badge>1</Badge>
@@ -21,7 +21,14 @@ export default function EntriesSection({ entries }) {
         {entries.map((entry, index) => (
           <Fragment key={entry.id}>
             {index > 0 ? <Divider /> : null}
-            <Entry date={entry.date} motto={entry.motto} notes={entry.notes} />
+            <Entry
+              date={entry.date}
+              motto={entry.motto}
+              notes={entry.notes}
+              onToggleFavorite={onToggleFavorite}
+              id={entry.id}
+              isFavorite={entry.isFavorite}
+            />
           </Fragment>
         ))}
       </div>

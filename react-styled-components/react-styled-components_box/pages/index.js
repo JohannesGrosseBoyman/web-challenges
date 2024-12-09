29fcd629d06ca502b45f2efc50c8bc00aa5b8f32
JohannesGrosseBoymann <dohecaf1219@gmail.com>
2024-12-09 17:42:28 +0100

    create BoxWithStyledComponent

--- react-styled-components/react-styled-components_box/pages/index.js
+++ react-styled-components/react-styled-components_box/pages/index.js
@@ -1,10 +1,22 @@
 import BoxWithClassName from "../components/BoxWithClassName/BoxWithClassName.js";
+import BoxWithStyledComponents from "@/components/BoxWithClassName/BoxWithStyledComponents.js";
+import styled from "styled-components";
+
+const StyledDiv = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 5px;
+`;
 
 export default function HomePage() {
   return (
-    <div>
-      <BoxWithClassName />
-      <BoxWithClassName isBlack />
-    </div>
+    <>
+      <StyledDiv>
+        <BoxWithClassName />
+        <BoxWithClassName isBlack />
+        <BoxWithStyledComponents />
+        <BoxWithStyledComponents $color="isBlack" />
+      </StyledDiv>
+    </>
   );
 }

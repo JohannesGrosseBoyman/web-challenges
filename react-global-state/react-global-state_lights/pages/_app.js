591d4f8d50e3e82cca1643b2f28d8aff06a93684
JohannesGrosseBoymann <dohecaf1219@gmail.com>
2024-12-11 18:24:05 +0100

    delete unnecessary props

--- react-global-state/react-global-state_lights/pages/_app.js
+++ react-global-state/react-global-state_lights/pages/_app.js
@@ -4,35 +4,36 @@ import { useState } from "react";
 
 export default function App({ Component, pageProps }) {
   const [lights, setLights] = useState([
-    { id: 0, name: "Living Room", isOn: false},
-    { id: 1, name: "Kitchen", isOn: false},
-    { id: 2, name: "Bedroom", isOn: false},
-    { id: 3, name: "Bathroom", isOn: false},
-    { id: 4, name: "Garage", isOn: false},
-    { id: 5, name: "Porch", isOn: false},
-    { id: 6, name: "Garden", isOn: false},
-    { id: 7, name: "Office", isOn: false},
-  ])
+    { id: 0, name: "Living Room", isOn: false },
+    { id: 1, name: "Kitchen", isOn: false },
+    { id: 2, name: "Bedroom", isOn: false },
+    { id: 3, name: "Bathroom", isOn: false },
+    { id: 4, name: "Garage", isOn: false },
+    { id: 5, name: "Porch", isOn: false },
+    { id: 6, name: "Garden", isOn: false },
+    { id: 7, name: "Office", isOn: false },
+  ]);
 
-  const lightsSum = lights.filter((room) =>
-    room.isOn === true ).length;
+  const lightsSum = lights.filter((room) => room.isOn === true).length;
   console.log("lights turned on: ", lightsSum);
 
-function handleToggle(id) {
-  setLights(
-    lights.map((room) => 
-    room.id === id ? { ...room, isOn: !room.isOn } : room 
-  )
-  );
-}
+  function handleToggle(id) {
+    setLights(
+      lights.map((room) =>
+        room.id === id ? { ...room, isOn: !room.isOn } : room
+      )
+    );
+  }
 
   return (
-    <Layout lightsSum={lightsSum} >
+    <Layout>
       <GlobalStyle />
-      <Component {...pageProps} lights={lights} 
-          onToggle={handleToggle} 
-          lightsSum={lightsSum}
-          />
+      <Component
+        {...pageProps}
+        lights={lights}
+        onToggle={handleToggle}
+        lightsSum={lightsSum}
+      />
     </Layout>
   );
 }

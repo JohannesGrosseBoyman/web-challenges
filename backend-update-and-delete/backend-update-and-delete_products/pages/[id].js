3cecc9808844d8ff6692b39adc6233938ce2a338
JohannesGrosseBoymann <dohecaf1219@gmail.com>
2024-12-20 13:36:01 +0100

    sending with problems remainin

--- backend-update-and-delete/backend-update-and-delete_products/pages/[id].js
+++ backend-update-and-delete/backend-update-and-delete_products/pages/[id].js
@@ -2,12 +2,40 @@ import useSWR from "swr";
 import styled from "styled-components";
 import { useRouter } from "next/router";
 import StyledLink from "@/components/Link";
+import { useState } from "react";
+import ProductForm from "@/components/ProductForm";
 
 export default function Product() {
   const router = useRouter();
   const { id } = router.query;
+  console.log("Id: ", id);
+  const { data, isLoading, mutate } = useSWR(`/api/products/${id}`);
+  const [isEditMode, setIsEditMode] = useState(false);
 
-  const { data, isLoading } = useSWR(`/api/products/${id}`);
+  async function handleEditProduct(event) {
+    console.log(event);
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const productData = Object.fromEntries(formData);
+
+    const response = await fetch(`/api/products/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(productData),
+    });
+
+    if (!response.ok) {
+      console.error(response.status);
+      return;
+    }
+    if(response.ok) {
+      console.log('test');
+    }
+    mutate();
+    setIsEditMode(!isEditMode);
+  }
 
   if (isLoading) {
     return <h1>Loading...</h1>;
@@ -25,6 +53,17 @@ export default function Product() {
         Price: {data.price} {data.currency}
       </p>
       <StyledLink href="/">Back to all</StyledLink>
+      <button
+        type="button"
+        onClick={() => {
+          setIsEditMode(!isEditMode);
+        }}
+      >
+        Edit the fish!
+      </button>
+      {isEditMode && (
+        <ProductForm onSubmit={handleEditProduct} isEditMode={isEditMode} />
+      )}
     </ProductCard>
   );
 }
--- backend-update-and-delete/backend-update-and-delete_products/pages/api/products/[id].js
+++ backend-update-and-delete/backend-update-and-delete_products/pages/api/products/[id].js
@@ -4,18 +4,28 @@ import Product from "@/db/models/Product";
 export default async function handler(request, response) {
   await dbConnect();
   const { id } = request.query;
+  try {
+    if (request.method === "GET") {
+      const product = await Product.findById(id).populate("reviews");
 
-  if (request.method === "GET") {
-    const product = await Product.findById(id).populate("reviews");
+      if (!product) {
+        response.status(404).json({ status: "Not Found" });
+        return;
+      }
 
-    if (!product) {
-      response.status(404).json({ status: "Not Found" });
+      response.status(200).json(product);
       return;
     }
 
-    response.status(200).json(product);
-    return;
-  }
+    if (request.method === "PUT") {
+      const updatedProduct = request.body;
+      await Product.findByIdAndUpdate(id, updatedProduct);
 
-  response.status(405).json({ status: "Method not allowed." });
+      response.status(200).json({ status: "Product successfully updated" });
+      return;
+    }
+  } catch (error) {
+    console.log("******", error);
+    // response.status(405).json({ status: error.message });
+  }
 }

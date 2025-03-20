a96cb23085773e118ed2ecee3bbb1910647b7c4b
JohannesGrosseBoymann <dohecaf1219@gmail.com>
2025-03-20 14:55:41 +0100

    create api for random-character, fetch data and display on homepage

--- backend-api-routes2/backend-api-routes_random-character/pages/index.js
+++ backend-api-routes2/backend-api-routes_random-character/pages/index.js
@@ -1,3 +1,24 @@
+import  useSWR  from "swr";
+
+const fetcher =  async (url) => {
+  const res = await fetch(url);
+  return res.json();
+};
+
 export default function HomePage() {
-  return <h1>Hello from Next.js!</h1>;
+  const { data, error } = useSWR("/api/random-character", fetcher);
+
+  if (error) return <h1>Failed to load!</h1>;
+
+  if (!data) return <h1>Loading...</h1>
+
+
+  return (
+    <div>
+      <h1>Random Character</h1>
+      <p>{data.firstName} {data.lastName} </p>
+      <p>twitter: {data.twitter}</p>
+      <p>geohash: {data.geohash}</p>
+    </div>
+  );
 }

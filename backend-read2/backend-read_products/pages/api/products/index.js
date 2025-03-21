5b5a819ea8d9441ba66042e946846d7e853db24e
JohannesGrosseBoymann <dohecaf1219@gmail.com>
2025-03-21 12:34:37 +0100

    add reviews to database and display

--- backend-read2/backend-read_products/pages/api/products/index.js
+++ backend-read2/backend-read_products/pages/api/products/index.js
@@ -1,5 +1,28 @@
-import { products } from "../../../lib/products";
+import dbConnect from "@/db/connect";
+import Product from "@/db/models/Product";
 
-export default function handler(request, response) {
-  return response.status(200).json(products);
+// handler function with error handling and error-log to the terminal console
+export default async function handler(request, response) {
+  try {
+
+    await dbConnect();
+
+
+    if (request.method === "GET") {
+
+      const products = await Product.find();
+
+
+      return response.status(200).json(products);
+    } else {
+      console.warn("Invalid request method:", request.method);
+      return response.status(405).json({ message: "Method not allowed" });
+    }
+  } catch (error) {
+    console.error("MongoDB Connection or Query Error:", error);
+    return response.status(500).json({
+      message: "Internal Server Error",
+      error: error.message,
+    });
+  }
 }

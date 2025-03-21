ae414f5725b5726ec5162397ad2854467ad1dbfd
JohannesGrosseBoymann <dohecaf1219@gmail.com>
2025-03-21 14:21:32 +0100

    add a POST route to api/products/index.js, send a POST request in Component ProductForm.js

--- backend-create2/backend-create_products/components/ProductForm.js
+++ backend-create2/backend-create_products/components/ProductForm.js
@@ -1,12 +1,38 @@
 import styled from "styled-components";
 import StyledButton from "@/components/Button";
+import useSWR, { mutate } from "swr";
 
 export default function ProductForm() {
+
+  const { mutate } = useSWR("/api/products");
+
   async function handleSubmit(event) {
     event.preventDefault();
 
-    const formData = new FormData(event.target);
-    const productData = Object.fromEntries(formData);
+    try {
+      const formData = new FormData(event.target);
+      const productData = Object.fromEntries(formData);
+
+      const response = await fetch("/api/products", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(productData),
+      });
+
+      //console.log("Response from POST: ", response);
+
+      if (response.ok) {
+        mutate();
+        event.target.reset();
+      } else {
+        console.error("Failed to create product", response.statusText);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+    
   }
 
   return (
--- backend-create2/backend-create_products/pages/api/products/index.js
+++ backend-create2/backend-create_products/pages/api/products/index.js
@@ -11,5 +11,21 @@ export default async function handler(request, response) {
     return;
   }
 
+  if (request.method === "POST") {
+    try {
+      const productData = request.body;
+      const newProduct = await Product.create(productData);
+
+      // console.log("Product created: ", newProduct);
+      response.status(201).json({ status: "Product created."});
+
+    } catch (error) {
+      console.error("Error creating new product: ", error);
+      response.status(400).json({ error: error.message});
+    }
+    return;
+  }
+
+
   response.status(405).json({ status: "Method not allowed." });
 }
